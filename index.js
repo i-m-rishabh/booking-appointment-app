f380681ad703a92642f0bf2e36b962d20e4ba52c
i-m-rishabh <uniquepandey125@gmail.com>
2023-05-03 00:07:03 +0530

    userData stored in form of object

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const myForm = document.querySelector('#myForm');
 // const email_input = document.querySelector('#email');
 // const btn = document.querySelector('#button');
 let list = document.querySelector('#list');
-
+// localStorage.setItem('userData',JSON.stringify({}));
 myForm.addEventListener('submit', onSubmit);
 
 function onSubmit(event){
@@ -20,7 +20,12 @@ function onSubmit(event){
         var text = document.createTextNode(name+" "+email);
         li.appendChild(text);
         list.appendChild(li);
-        localStorage.setItem(name,email);
+        // storing user data in form of key value pair //
+        // localStorage.setItem(name,email);
+        // storing user data in form of object //
+        var userData = JSON.parse(localStorage.getItem('userData'));
+        userData = {...userData,[name]:email};
+        localStorage.setItem('userData',JSON.stringify(userData));
         event.target.name.value = '';
         event.target.email.value = '';
     }
